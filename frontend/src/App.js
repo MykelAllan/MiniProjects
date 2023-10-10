20537d7de9d0c51bfb7abc19605d7d6a1f8eded0
MykelAllan <michaelallanlomugdang@gmail.com>
2023-10-10 10:00:15 -0400

    initial change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
 // Functional component
 const App = () => {
   const [users, setUsers] = useState([]); // State for users
+  const [newUser, setNewUser] = useState("");
 
-  // Fetch users from API
-  useEffect(() => {
-    axios.get('<http://localhost:3000/api/users>')
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  // Event handler for refreshing users
-  const handleRefresh = () => {
-    // Fetch users again
-    axios.get('<http://localhost:3000/api/users>')
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  };
-
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    setUsers([...users, newUser]);
+    setNewUser("");
+  }
   return (
     <div>
-      <h1>User List</h1>
-      <button onClick={handleRefresh}>Refresh</button>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <h1>Add User Form</h1>
+      <form onSubmit={addUserHandler}>
+        <input
+          type="text"
+          placeholder="Insert User"
+          value={newUser}
+          onChange={(e) => setNewUser(e.target.value)} 
+        />
+        <button type="submit" >Add User</button>
+
+      </form>
+      <DisplayUsers users={users}/>
     </div>
   );
 };
+
+//Class component with props
+function DisplayUsers(props) {
+  return(
+<div>
+  <h1>List of Users</h1>
+  {/* Condition if users length is greater than 0  
+      show the user if not then show "No User To Show"
+  */}
+  {props.users.length > 0 ? 
+  (
+  <div>
+      {props.users.map((user, index) => (
+        <li key={index}>{user}</li>
+      ))}
+    </div>
+  ) : 
+  (
+    <div>
+      <h4>No Users to Show</h4>
+      </div>
+  )}
+</div>
+  );
+}
 
 export default App;
